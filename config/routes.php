783e78362d533f22bcccfe0d715b832ee3f02a86
Eren <nouman.laravel@outlook.com>
2026-01-15 22:08:34 +0800

    routes

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,6 @@
 return [
     "verify_emails" => '/email/verify/notice',
     "verify_emails_hash" => '/email/verify/{id}/{hash}',
-    "verify_emails_local" => '/email/verify/{id}/{hash}',
+    "verify_emails_local" => '/email/verify/{id}/{hash}/hmm',
     "en" => '/email/verification-notification',
 ];
